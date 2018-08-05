841016bd2c638568218c8cd4d236130d1e338164
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-08-06 01:13:18 +0300

    Set JSON as default for request, response and body_parsers

--- apps/api/application.rb
+++ apps/api/application.rb
@@ -92,12 +92,12 @@ module Api
       # Default format for the requests that don't specify an HTTP_ACCEPT header
       # Argument: A symbol representation of a mime type, defaults to :html
       #
-      # default_request_format :html
+      default_request_format :json
 
       # Default format for responses that don't consider the request format
       # Argument: A symbol representation of a mime type, defaults to :html
       #
-      # default_response_format :html
+      default_response_format :json
 
       # HTTP Body parsers
       # Parse non GET responses body for a specific mime type
@@ -105,7 +105,7 @@ module Api
       #             (only `:json` is supported)
       #           Object, the parser
       #
-      # body_parsers :json
+      body_parsers :json
 
       # When it's true and the router receives a non-encrypted request (http),
       # it redirects to the secure equivalent (https). Disabled by default.
