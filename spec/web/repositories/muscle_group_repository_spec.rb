76b4c0b5d22ed27a41bb953504f832ca047e3998
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-08-11 00:35:26 +0300

    Add sorting muscle groups by name ascending

--- spec/web/repositories/muscle_group_repository_spec.rb
+++ spec/web/repositories/muscle_group_repository_spec.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 RSpec.describe MuscleGroupRepository, type: :repository do
-  # place your tests here
+  let(:repository) { MuscleGroupRepository.new }
+
+  describe '#sorted' do
+    it 'returns sorted ascending by name' do
+      items = %w[legs ноги arms руки chest грудь]
+      items.each { |item| repository.create(name: item) }
+
+      expect(repository.sorted.map(&:name)).to eq(items.sort)
+    end
+  end
 end
