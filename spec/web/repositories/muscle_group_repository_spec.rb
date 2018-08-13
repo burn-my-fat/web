c7deeac3d8edc44d827582402e9375f58ab07591
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-08-13 23:51:16 +0300

    Replace muscles calculation with lambda

--- spec/web/repositories/muscle_group_repository_spec.rb
+++ spec/web/repositories/muscle_group_repository_spec.rb
@@ -3,6 +3,7 @@
 RSpec.describe MuscleGroupRepository, type: :repository do
   let(:repository) { MuscleGroupRepository.new }
   let(:muscle_group) { repository.create(name: 'Legs') }
+  let(:muscles) { -> { repository.find_with_muscles(muscle_group.id).muscles } }
 
   describe '#sorted' do
     it 'returns sorted ascending by name' do
@@ -16,7 +17,7 @@ RSpec.describe MuscleGroupRepository, type: :repository do
   describe '#find_with_muscles' do
     context 'when muscles do not exist' do
       it 'returns empty array' do
-        expect(repository.find_with_muscles(muscle_group.id).muscles).to be_empty
+        expect(muscles.call).to be_empty
       end
     end
 
@@ -26,7 +27,7 @@ RSpec.describe MuscleGroupRepository, type: :repository do
         muscle_repository.create(name: 'Quadriceps Femoris', muscle_group_id: muscle_group.id)
         muscle_repository.create(name: 'Adductor', muscle_group_id: muscle_group.id)
 
-        expect(repository.find_with_muscles(muscle_group.id).muscles.size).to eq(2)
+        expect(muscles.call.size).to eq(2)
       end
     end
   end
@@ -34,17 +35,17 @@ RSpec.describe MuscleGroupRepository, type: :repository do
   describe '#add_muscle' do
     it 'adds muscle to muscle group' do
       repository.add_muscle(muscle_group, name: 'Quadriceps Femoris')
-      expect(repository.find_with_muscles(muscle_group.id).muscles.size).to eq(1)
+      expect(muscles.call.size).to eq(1)
     end
   end
 
   describe '#remove_muscle' do
     it 'removes muscle from muscle group' do
       muscle = repository.add_muscle(muscle_group, name: 'Quadriceps Femoris')
-      expect(repository.find_with_muscles(muscle_group.id).muscles.size).to eq(1)
+      expect(muscles.call.size).to eq(1)
 
       repository.remove_muscle(muscle_group, muscle.id)
-      expect(repository.find_with_muscles(muscle_group.id).muscles.size).to eq(0)
+      expect(muscles.call.size).to eq(0)
     end
   end
 end
