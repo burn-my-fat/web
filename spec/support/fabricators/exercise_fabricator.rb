21a38595ca413df8448c0f28913283d232093741
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-11-08 07:04:40 +0300

    Add ExerciseTargetMuscle model

--- spec/support/fabricators/exercise_fabricator.rb
+++ spec/support/fabricators/exercise_fabricator.rb
@@ -4,6 +4,6 @@ Fabricator(:exercise) do
   name { FFaker::Lorem.words(2) }
   description { FFaker::Lorem.paragraph }
 
-  training_type { Fabricate(:training_type) }
-  muscle_group { Fabricate(:muscle_group) }
+  training_type_id { Fabricate(:training_type).id }
+  muscle_group_id { Fabricate(:muscle_group).id }
 end
